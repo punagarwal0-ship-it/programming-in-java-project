d904ee96cf774d5cd34cfcab48c9cacd07b54551
punagarwal0-ship-it <punagarwal0@gmail.com>
2026-09-18 16:56:40 +0530

    Supplier class

--- InventorySystem_Puneet_Agarwal/src/ims/Supplier.java
+++ InventorySystem_Puneet_Agarwal/src/ims/Supplier.java
@@ -1,7 +1,19 @@
 package ims;
 public class Supplier {
     private int id; private String name; private String phone;
-    public Supplier(int id,String name,String phone){if(id<=0)throw new IllegalArgumentException("Supplier ID must be positive.");if(name==null||name.isBlank())throw new IllegalArgumentException("Supplier name cannot be empty.");this.id=id;this.name=name;this.phone=phone;}
-    public int getId(){return id;} public String getName(){return name;} public String getPhone(){return phone;}
-    public String toString(){return "ID: "+id+" | Name: "+name+" | Phone: "+phone;}
+    public Supplier(int id,String name,String phone){
+        if(id<=0)throw new IllegalArgumentException("Supplier ID must be positive.");
+        if(name==null||name.isBlank())throw new IllegalArgumentException("Supplier name cannot be empty.");
+        this.id=id;
+        this.name=name;
+        this.phone=phone;}
+    
+    public int getId()    {
+        return id;    } 
+    public String getName(){
+        return name;} 
+    public String getPhone(){
+        return phone;}
+    public String toString(){
+        return "ID: "+id+" | Name: "+name+" | Phone: "+phone;}
 }

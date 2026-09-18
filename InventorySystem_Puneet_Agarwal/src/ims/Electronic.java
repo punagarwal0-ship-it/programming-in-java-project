a891d5e5f2fd1606e49e8b44b6d1a7abd773dab0
punagarwal0-ship-it <punagarwal0@gmail.com>
2026-09-18 16:43:03 +0530

    Electronic constructor and methods

--- InventorySystem_Puneet_Agarwal/src/ims/Electronic.java
+++ InventorySystem_Puneet_Agarwal/src/ims/Electronic.java
@@ -1,6 +1,12 @@
 package ims;
 public class Electronic extends Product {
     private String brand;
-    public Electronic(int id,String name,double price,int quantity,int minimumStock,String brand){super(id,name,price,quantity,minimumStock);this.brand=brand;}
-    public String getBrand(){return brand;} public String getType(){return "Electronic";}
+    public Electronic(int id,String name,double price,int quantity,int minimumStock,String brand)
+    {
+        super(id,name,price,quantity,minimumStock);this.brand=brand;
+    }
+    public String getBrand(){return brand;} public String getType()
+    {
+        return "Electronic";
+    }
 }
